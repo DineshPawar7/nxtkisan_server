236d2317205894bba6eddb020ef8ad61c5ecff31
Dinesh Pawar <dsp70458@gmail.com>
2025-05-13 08:50:42 +0530

    remove unused controllers, models, middleware, and routes; update nodemon version

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getMe, getUserProfile } = require('../controllers/userController');
-const { protect } = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.get('/me', protect, getMe);
-router.get('/:username', getUserProfile);
-
-module.exports = router;
